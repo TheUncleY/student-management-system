bab0b77e4221b05518c9f05298af769439722827
TheUncleY <49539481+TheUncleY@users.noreply.github.com>
2023-05-19 10:54:51 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/StudentManagementSystemApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.entity.Student;
+import com.example.demo.repository.StudentRepository;
+
+
+
+@SpringBootApplication
+public class StudentManagementSystemApplication  implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemApplication.class, args);
+	}
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+//		
+//		 Student student1 = new Student("Ramesh", "Fadatare", "ramesh@gmail.com");
+//		 studentRepository.save(student1);
+//		 
+//		 Student student2 = new Student("Sanjay", "Jadhav", "sanjay@gmail.com");
+//		 studentRepository.save(student2);
+//		 
+//		 Student student3 = new Student("tony", "stark", "tony@gmail.com");
+//		 studentRepository.save(student3);
+//		 
+		
+	}
+
+}
